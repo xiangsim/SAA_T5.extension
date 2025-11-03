a372814fa5f452b4c4905da8f645652cdb96b569
xiangsim <sim.jk@outlook.com>
2025-11-03 13:51:51 +0800

    roomwarning

--- SAA_T5.tab/Settings.panel/col1.stack/Room Warning Dismissal.smartbutton/script.py
+++ SAA_T5.tab/Settings.panel/col1.stack/Room Warning Dismissal.smartbutton/script.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 __title__ = "RoomWarning"
 __author__ = "JK_Sim"
-__doc__ = """Version = 1.0
-Date    = 30.10.2025
+__doc__ = """Version = 1.1
+Date    = 03.11.2025
 _____________________________________________________________________
 Description:
-
-Dismiss Multiple Rooms Warning
+Dismiss Multiple Rooms Warning and show alert when user clicks
 _____________________________________________________________________
 How-to:
-
--> Always ON on startup
+-> Always ON on startup (registers silently)
+-> Alert shown on manual button click
 _____________________________________________________________________
 """
 
@@ -23,25 +22,18 @@ from Autodesk.Revit.DB import FailureProcessingResult
 from Autodesk.Revit.DB.Events import FailuresProcessingEventArgs
 from System import EventHandler
 
-# pyRevit expects this for .smartbutton, even if empty
-def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
-    pass
-
-
 # Keep reference to prevent garbage collection
 event_handler = None
 
-
 def handle_failures(sender, args):
-    """Event handler that dismisses specified warnings"""
     fa = args.GetFailuresAccessor()
     failures = fa.GetFailureMessages()
     to_delete = []
 
     for fm in failures:
-        desc = fm.GetDescriptionText()
-        if ("Multiple Rooms are in the same enclosed region" in desc or
-            "Room Tag is outside of its Room" in desc):
+        msg = fm.GetDescriptionText()
+        if ("Multiple Rooms are in the same enclosed region" in msg or
+            "Room Tag is outside of its Room" in msg):
             to_delete.append(fm)
 
     for fm in to_delete:
@@ -49,8 +41,8 @@ def handle_failures(sender, args):
 
     args.SetProcessingResult(FailureProcessingResult.Continue)
 
-
-def enable_room_warning_handler():
+def register_handler():
+    """Register failure handler silently"""
     global event_handler
     app = HOST_APP.app
 
@@ -58,8 +50,14 @@ def enable_room_warning_handler():
         event_handler = EventHandler[FailuresProcessingEventArgs](handle_failures)
         app.FailuresProcessing += event_handler
 
+def enable_room_warning_handler():
+    """Used when user clicks the button: shows alert and ensures handler is on"""
+    register_handler()
     forms.alert("Room warning dismissal is ON", title="Dismiss Warning")
 
+# Called by pyRevit when extension loads
+def __selfinit__(script_cmp, ui_button_cmp, __rvt__):
+    register_handler()
 
-if __name__ == "__main__":
-    enable_room_warning_handler()
+# Called when user clicks the button manually
+enable_room_warning_handler()
